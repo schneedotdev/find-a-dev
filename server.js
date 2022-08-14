5792d656df5c042af845cceb69a61dd9a2d29309
brianschnee <brian.a.schnee@gmail.com>
2022-08-13 20:26:52 -0400

    dynamically render dev from db

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ MongoClient.connect(DB_CONNECT, { useUnifiedTopology: true })
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.get('/', (req, res) => {
